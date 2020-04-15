d35da64cde39d96dfaddb5d5e0102f6943268c65
jaco9419 <40173724+jaco9419@users.noreply.github.com>
2020-04-15 11:07:52 -0300

    Updates

---
+++ precache-manifest.98ec2a21e7e0ce7b0e363dedaa2e7948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a949a9f6912e035147befb5fde59228",
+    "url": "/tic-tac-toe-2/index.html"
+  },
+  {
+    "revision": "045cfa3ca0849cb016c8",
+    "url": "/tic-tac-toe-2/static/css/main.e47f73f1.chunk.css"
+  },
+  {
+    "revision": "5093645f8182b350f830",
+    "url": "/tic-tac-toe-2/static/js/2.5ebbd700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-2/static/js/2.5ebbd700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045cfa3ca0849cb016c8",
+    "url": "/tic-tac-toe-2/static/js/main.0da20ef5.chunk.js"
+  },
+  {
+    "revision": "52d4a216553f0f052ba6",
+    "url": "/tic-tac-toe-2/static/js/runtime-main.c1d1be49.js"
+  }
+]);
